adff0a6c042f3054483ad7f1a10b439687b82dd1
Paulo Bittencourt <62356712+Paulobittencourts@users.noreply.github.com>
2022-11-21 22:40:03 -0300

    Add files via upload

--- Figurinhas/public/config/config.js
+++ Figurinhas/public/config/config.js
@@ -1,10 +1,11 @@
 const path = require('path')
+require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "admin",
-    "database": "database_development",
+    "username": process.env.USERNAME_BD,
+    "password": process.env.PASSWORD_BD,
+    "database": process.env.NOME_BANCO,
     "host": "127.0.0.1",
     "dialect": "mysql",
     "migration-path": path.resolve(__dirname, 'migrations')
--- Figurinhas/public/router.js
+++ Figurinhas/public/router.js
@@ -8,10 +8,14 @@ const Figurinhas = require('./Controlles/FigurinhasController')
 const Users = require('./models/User')
 
 router.post('/users', UserController.store)
-router.get('/users', UserController.index)
+router.get('/users', UserController.checkToken, UserController.index)
+router.post('/users/auth', UserController.login)
+router.get('/users/:id', UserController.checkToken, UserController.auth)
+router.delete('/users/:id', UserController.checkToken, UserController.delete)
 
-router.post('/users/:user_id/figurinhas', Figurinhas.store)
-router.get('/users/:user_id/figurinhas', Figurinhas.index)
+
+router.post('/users/:user_id/figurinhas',UserController.checkToken, Figurinhas.store)
+router.get('/users/:user_id/figurinhas',UserController.checkToken, Figurinhas.index)
 
 
 module.exports = router
