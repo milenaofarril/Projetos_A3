651b4fac01d13355a860fef8635ec223cd1aba77
Paulo Bittencourt <62356712+Paulobittencourts@users.noreply.github.com>
2022-11-27 14:01:15 -0300

    Add files via upload

---
+++ Figurinhas/public/config/config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "admin",
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "migration-path": path.resolve(__dirname, 'migrations')
+  },
+
+  "Production": {
+    "username": "root",
+    "password": "admin",
+    "database": "Figurinhas",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "migration-path": path.resolve(__dirname, 'migrations')
+  }
+}
